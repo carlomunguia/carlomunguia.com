b78c1fd66b8156d28dbd4368fd333cba48a06c93
Carlo Munguia <carlomunguia@carlos-air.home>
2019-02-16 21:35:27 -0600

    connecting new build

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,7 @@
 <ul class="card card-body mt-5 shadow-lg">
   <li class="btn btn-primary btn-lg text-center">SOFTWARE DEVELOPER/PROGRAMMER/JOB-SEEKER</li>
   <li class="btn btn-primary btn-lg btn-danger">EMAIL: carlo.munguia.82@gmail.com</li>
-  <li class="btn btn-primary btn-lg btn-warning">CELL: 281.898.8279 PRIMARY</li>
+  <li class="btn btn-primary btn-lg btn-warning">CELL: 281.898.8279</li>
   <a href="https://github.com/carlomunguia" class="btn btn-primary btn-lg btn-info">GIT</a>
 </ul>
   </div>
