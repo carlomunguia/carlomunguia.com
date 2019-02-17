6de7d8427f263df7e11426d5719f57443b80d784
Carlo Munguia <carlomunguia@carlos-air.home>
2019-02-16 22:08:06 -0600

    added more content to buttons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,8 +3,10 @@
   <div class="contentArea shadow-lg">
 <ul class="card card-body mt-5 shadow-lg">
   <li class="btn btn-primary btn-lg text-center">SOFTWARE DEVELOPER/PROGRAMMER/JOB-SEEKER</li>
-  <li class="btn btn-primary btn-lg btn-danger"></li>
-  <li class="btn btn-primary btn-lg btn-warning"></li>
+  <li class="btn btn-primary btn-lg btn-danger">LANGUAGES: CSS/GO/HTML/JAVA/JAVASCRIPT/JULIA/PYTHON/RUBY/SPRING/SQL/SWIFT</li>
+  <li class="btn btn-primary btn-lg btn-warning">FRAMEWORKS(FRONT):BOOTSTRAP/DJANGO/FLASK/HANAMI/HIBERNATE/RAILS/ </li>
+  <li class="btn btn-primary btn-lg btn-secondary">FRAMEWORKS(BACK):MYSQL/JQUERY/PHP</li>
+
   <a href="https://github.com/carlomunguia" class="btn btn-primary btn-lg btn-info">GIT</a>
 </ul>
   </div>
