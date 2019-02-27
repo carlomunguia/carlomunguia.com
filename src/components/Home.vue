ec1124d6af767d2829c5dd2a1d5148f141df9280
Carlo Munguia <carlomunguia@carlos-air.home>
2019-02-27 16:31:53 -0600

    bug fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,55 +1,55 @@
 <template>
-  <div>
-    <div class="contentArea">
-      <ul class="card card-body mt-5 ml-5 bg-dark">
-        <li class="btn btn-primary btn-lg ">SOFTWARE DEVELOPER/PROGRAMMER/JOB-SEEKER</li>
-        <li class="btn btn-warning btn-lg ">LANGUAGES: CSS/ERLANG/GO/HASKELL/HTML/JAVA/JAVASCRIPT/RUBY</li>
-        <li class="btn btn-secondary btn-lg ">FRAMEWORKS(FRONT):BOOTSTRAP/DJANGO/FLASK/HANAMI/HIBERNATE/RAILS/</li>
-        <li class="btn btn-danger btn-lg ">FRAMEWORKS(BACK):NPM/JQUERY/PHP</li>
-        <a href="https://github.com/carlomunguia" class="btn btn-primary">GIT</a>
-      </ul>
-    </div>
-    <section class="contentArea">
-      <div class="card card-deck mt-5 ml-4 bg-dark w-100">
-        <div class="card bg-dark">
-          <div class="card-body bg-primary">
-            <h4 class="card-title">BINARY TREE OF LIFE</h4>
-            <p class="card-text">
-              LEARNING THE KEY PAIR VALUE OF LIFE - VIA CODING, VIA PURE SYNTAX
-            </p>
-          </div>
-        </div>
-        <div class="card bg-dark">
-          <div class="card-body bg-danger">
-            <h4 class="card-title">A CHANCE TO TRANSFORM</h4>
-            <p class="card-text">
-              ONE BYTE, ONE SLICE, ONE ITERATION AT A TIME - THIS WORLD WILL BE CHANGED!
-            </p>
-          </div>
-        </div>
-        <div class="card bg-dark">
-          <div class="card-body bg-warning">
-            <h4 class="card-title">A FUTURIST WITH A SENSE OF SPIRIT</h4>
-            <p class="card-text">PURE CODE CASCADING AND FLOWING TO US ALL - ALWAYS! 2019 IS A DIFFERENT WORLD </p>
-          </div>
+<div>
+  <div class="contentArea">
+<ul class="card card-body mt-5 ml-4 bg-dark">
+  <li class="btn btn-primary btn-lg ">SOFTWARE DEVELOPER/PROGRAMMER/JOB-SEEKER</li>
+  <li class="btn btn-warning btn-lg ">LANGUAGES: CSS/ERLANG/GO/HASKELL/HTML/JAVA/JAVASCRIPT/RUBY</li>
+  <li class="btn btn-secondary btn-lg ">FRAMEWORKS(FRONT):BOOTSTRAP/DJANGO/FLASK/HANAMI/HIBERNATE/RAILS/ </li>
+  <li class="btn btn-danger btn-lg ">FRAMEWORKS(BACK):NPM/JQUERY/PHP</li>
+  <a href="https://github.com/carlomunguia" class="btn btn-primary">GIT</a>
+</ul>
+  </div>
+  <section class="contentArea">
+    <div class="card card-deck m-5 ml-4 bg-dark bg-dark">
+      <div class="card">
+        <div class="card-body bg-primary">
+          <h4 class="card-title">BINARY TREE OF LIFE</h4>
+          <p class="card-text">
+            LEARNING THE KEY PAIR VALUE OF LIFE - VIA CODING, VIA PURE SYNTAX
+          </p>
         </div>
       </div>
-    </section>
-    <span>
+      <div class="card">
+        <div class="card-body bg-danger">
+          <h4 class="card-title">A CHANCE TO TRANSFORM</h4>
+          <p class="card-text">
+            ONE BYTE, ONE SLICE, ONE ITERATION AT A TIME - THIS WORLD WILL BE CHANGED!
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-body bg-warning">
+          <h4 class="card-title">A FUTURIST WITH A SENSE OF SPIRIT</h4>
+          <p class="card-text">PURE CODE CASCADING AND FLOWING TO US ALL - ALWAYS! 2019 IS A DIFFERENT WORLD </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <span>
         <br>
     </span>
-  </div>
+</div>
 </template>
 
 <script>
-  export default {
-    name: 'Home',
-    data() {
-      return {
-        msg: 'This is Home'
-      }
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'This is Home'
     }
   }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
