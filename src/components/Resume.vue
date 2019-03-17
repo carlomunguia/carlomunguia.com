5833ad8895826c93b51a08c7c4e694414520939d
Carlo Munguia <carlomunguia@carlos-air.home>
2019-03-17 13:18:28 -0500

    adding a bit more content more next phase of resume work

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -2,17 +2,21 @@
   <div class="container content-area w-100">
     <h1 class="btn text-center btn-secondary text-white align-middle">carlo munguia :: full-stack web designer</h1>
     <div class=" bg-secondary justify-content-left">
-      <h2 class="btn btn-success">language-stack</h2>
+      <h2 class="btn btn-success">language stack</h2>
       <ul class="bg-secondary">
         <li class="btn font-weight-bold btn-warning">css</li>
         <li class="btn font-weight-bold btn-primary">html</li>
         <li class="btn font-weight-bold btn-warning">go</li>
-        <li class="btn font-weight-bold btn-primary ">java</li>
+        <li class="btn font-weight-bold btn-primary">java</li>
         <li class="btn font-weight-bold btn-warning">javascript</li>
         <li class="btn font-weight-bold btn-primary">ruby</li>
+        <li class="btn font-weight-bold btn-warning">rust</li>
+        <li class="btn font-weight-bold btn-primary">sass</li>
+
+
       </ul>
       <div class="bg-secondary content-area">
-        <h2 class="btn btn-success">front-end frameworks</h2>
+        <h2 class="btn btn-success">front-end stack</h2>
         <ul class="bg-secondary">
           <li class="btn btn-primary font-weight-bold">bootstrap</li>
           <li class="btn btn-warning font-weight-bold">ember</li>
@@ -31,9 +35,10 @@
           <li class="btn btn-primary font-weight-bold">broccoli</li>
           <li class="btn btn-warning font-weight-bold">cocoa-pods</li>
           <li class="btn btn-primary font-weight-bold">npm</li>
-          <li class="btn btn-warning font-weight-bold">postgres-sql</li>
-          <li class="btn btn-primary font-weight-bold">sql</li>
-          <li class="btn btn-warning font-weight-bold">yarn</li>
+          <li class="btn btn-warning font-weight-bold">nginx</li>
+          <li class="btn btn-primary font-weight-bold">postgres-sql</li>
+          <li class="btn btn-warning font-weight-bold">sql</li>
+          <li class="btn btn-primary font-weight-bold">yarn</li>
         </ul>
       </div>
 
