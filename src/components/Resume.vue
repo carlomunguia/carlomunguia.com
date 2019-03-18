a66b6b9877dcf0c18df40c1bc524f1026c7bb100
Carlo Munguia <carlomunguia@carlos-air.home>
2019-03-17 23:15:53 -0500

    building out the resume with more of my skills & technologies

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -1,46 +1,75 @@
 <template>
   <div class="container content-area w-100">
-    <h1 class="btn text-center btn-outline-warning text-white">carlo munguia :: full-stack web designer</h1>
+    <div class="btn btn-warning">address</div>
+    <div class="btn btn-warning">email</div>
+    <div class="btn btn-warning">mobile</div>
+    <div class="btn btn-warning">git repo</div>
+    <div class="btn btn-warning">ember.js meetup</div>
+    <h1 class="btn-lg text-center text-white float-left">carlo munguia :: full-stack web designer</h1>
+    <br>
+    <br>
+    <h2 class="btn-lg text-center text-white">summary</h2>
+    <p class="content-area card-body">
+      As an electrical conduit brought to life by a mutation within the node channel in the 9th dimension, I know something about programming.
+    </p>
     <div class=" justify-content-left">
-      <h2 class="btn btn-outline-warning text-white ml-5">language stack</h2>
-      <ul class="">
-        <li class="btn font-weight-bold btn-warning">css</li>
-        <li class="btn font-weight-bold btn-primary">html</li>
-        <li class="btn font-weight-bold btn-warning">go</li>
-        <li class="btn font-weight-bold btn-primary">java</li>
-        <li class="btn font-weight-bold btn-warning">javascript</li>
-        <li class="btn font-weight-bold btn-primary">ruby</li>
-        <li class="btn font-weight-bold btn-warning">rust</li>
-        <li class="btn font-weight-bold btn-primary">sass</li>
+      <h2 class="btn text-left text-white text-center">languages</h2>
+      <ul class="ml-5">
+        <li class="btn font-weight-bold btn-secondary">css3</li>
+        <li class="btn font-weight-bold btn-secondary">html</li>
+        <li class="btn font-weight-bold btn-secondary">go</li>
+        <li class="btn font-weight-bold btn-secondary">handlebars</li>
+        <li class="btn font-weight-bold btn-secondary">java</li>
+        <li class="btn font-weight-bold btn-secondary">javascript</li>
+        <li class="btn font-weight-bold btn-secondary">ruby</li>
+        <li class="btn font-weight-bold btn-secondary">rust</li>
+        <li class="btn font-weight-bold btn-secondary">sass</li>
+        <li class="btn font-weight-bold btn-secondary">swift</li>
+
       </ul>
       <div class="content-area">
-        <h2 class="btn btn-outline-warning text-white ml-5">front-end stack</h2>
-        <ul class="">
-          <li class="btn btn-primary font-weight-bold">bootstrap</li>
-          <li class="btn btn-warning font-weight-bold">ember</li>
-          <li class="btn btn-primary font-weight-bold">jQuery</li>
-          <li class="btn btn-warning font-weight-bold">node.js</li>
-          <li class="btn btn-primary font-weight-bold">rails</li>
-          <li class="btn btn-warning font-weight-bold">react</li>
-          <li class="btn btn-primary font-weight-bold">spring</li>
-          <li class="btn btn-warning font-weight-bold">vue</li>
+        <h2 class="btn text-left text-white">frameworks</h2>
+        <ul class="ml-5">
+          <li class="btn btn-success font-weight-bold">bootstrap</li>
+          <li class="btn btn-success font-weight-bold">ember</li>
+          <li class="btn btn-success font-weight-bold">jQuery</li>
+          <li class="btn btn-success font-weight-bold">node.js</li>
+          <li class="btn btn-success font-weight-bold">p5.js</li>
+          <li class="btn btn-success font-weight-bold">rails</li>
+          <li class="btn btn-success font-weight-bold">react</li>
+          <li class="btn btn-success font-weight-bold">spring</li>
+          <li class="btn btn-success font-weight-bold">vue</li>
         </ul>
       </div>
       <div class="content-area">
-        <h2 class="btn btn-outline-warning text-left text-white ml-5">back-end stack</h2>
-        <ul class="">
-          <li class="btn btn-warning font-weight-bold">apache</li>
-          <li class="btn btn-primary font-weight-bold">broccoli</li>
-          <li class="btn btn-warning font-weight-bold">cocoa-pods</li>
-          <li class="btn btn-primary font-weight-bold">npm</li>
-          <li class="btn btn-warning font-weight-bold">nginx</li>
-          <li class="btn btn-primary font-weight-bold">postgres-sql</li>
-          <li class="btn btn-warning font-weight-bold">sql</li>
-          <li class="btn btn-primary font-weight-bold">yarn</li>
+        <h2 class="btn text-left text-white">systems</h2>
+        <ul class="ml-5">
+          <li class="btn btn-secondary font-weight-bold">aws</li>
+          <li class="btn btn-secondary font-weight-bold">broccoli</li>
+          <li class="btn btn-secondary font-weight-bold">cocoa-pods</li>
+          <li class="btn btn-secondary font-weight-bold">digital ocean</li>
+          <li class="btn btn-secondary font-weight-bold">npm</li>
+          <li class="btn btn-secondary font-weight-bold">nginx</li>
+          <li class="btn btn-secondary font-weight-bold">postgres-sql</li>
+          <li class="btn btn-secondary font-weight-bold">sql</li>
+          <li class="btn btn-secondary font-weight-bold">yarn</li>
         </ul>
       </div>
       <div class="content-area">
-        <h1 class="btn text-center btn-outline-warning text-white">work history :: present to past</h1>
+        <h2 class="btn text-left text-white">tools</h2>
+        <ul class="ml-5">
+          <li class="btn btn-success font-weight-bold">intellij</li>
+          <li class="btn btn-success font-weight-bold">rubymine</li>
+          <li class="btn btn-success font-weight-bold">goland</li>
+          <li class="btn btn-success font-weight-bold">atom</li>
+          <li class="btn btn-success font-weight-bold">git</li>
+          <li class="btn btn-success font-weight-bold">processing</li>
+          <li class="btn btn-success font-weight-bold">x-code</li>
+          <li class="btn btn-success font-weight-bold">vs-code</li>
+        </ul>
+      </div>
+      <div class="content-area">
+        <h1 class="btn text-center text-white">work history :: present to past</h1>
       </div>
     </div>
   </div>
